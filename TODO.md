7df9530d9e1ed00e643bc93482d4f46190b718a0
Ivy O'Neal-Odom <ivyoo@protonmail.com>
2023-10-30 11:57:27 -0500

    Basic chat messages for items implemented

--- TODO.md
+++ TODO.md
@@ -1,6 +1,6 @@
+# Bug
+
 # Current List
- - make item types have meaningful chat messages when rolled
- - compendia of skills, spells, level features, etc.
  - UI overhaul
 
 # Future list
@@ -8,6 +8,8 @@ Character sheet
  - rework items to be Foundry Items, not text boxes?
  - replace textarea with TinyMCE editors
 
+
+- compendia of skills, spells, level features, etc.
  
 - make universal grind
 - make universal conflict page
--- module/documents/item.mjs
+++ module/documents/item.mjs
@@ -40,147 +40,180 @@ export class TorchbearerItem extends Item {
     const label = `[${item.type}] ${item.name}`;
 
     //Make the calculations for skill rolls
-    if(this.type === "Skill"){
-      let dialog_content;
-      let dataset = {
-        "baseDice": 0,
-        "label": item.name
-      };
+    switch (this.type) {
 
-      if(this.system.beginners_luck){
-        //Beginners Luck Stuff
-        if(Object.keys(this.system.based_on_options).includes(this.system.based_on)){
-          dataset.baseDice = this.parent.system.abilities.raw_abilities[this.system.based_on].rating
-        }
+      case "Skill":
+        let dialog_content;
+        let dataset = {
+          "baseDice": 0,
+          "label": item.name
+        };
 
-        dialog_content = await renderTemplate("systems/ivys_torchbearer/templates/rolls/roll-build-dialog-beginners_luck.hbs", dataset);
-      }else{        
-        dataset.baseDice = this.system.rating
-      
-        dialog_content = await renderTemplate("systems/ivys_torchbearer/templates/rolls/roll-build-dialog.hbs", dataset);
-      }
-
-
-      new Dialog({
-        title: "Rolling " + this.name,
-        content: dialog_content,
-        buttons: {
-          button1: {
-            label: "Roll",
-            callback: (html) => roll_dialog_callback(html, dataset, this.actor, this.system.beginners_luck),
-            icon: `<i class="fas fa-check"></i>`
-          },
-          button2: {
-            label: "Cancel",
-            callback: () => {},
-            icon: `<i class="fas fa-times"></i>`
+        if(this.system.beginners_luck){
+          //Beginners Luck Stuff
+          if(Object.keys(this.system.based_on_options).includes(this.system.based_on)){
+            dataset.baseDice = this.parent.system.abilities.raw_abilities[this.system.based_on].rating
           }
-        }
-      }).render(true);
 
-      async function roll_dialog_callback(html, roll_dialog_data, actor, beginners_luck){
-        const ob = parseInt(html.find("input#ob").val());
-        const miscDice = parseInt(html.find("input#miscDice").val());
-        const miscSuccesses = parseInt(html.find("input#miscSuccesses").val());
-        const baseDice = parseInt(roll_dialog_data.baseDice);
-        let totalDice = 0;
-        if(beginners_luck){
-          const miscDiceHalved = parseInt(html.find("input#miscDiceHalved").val());
-          totalDice = Math.ceil((baseDice + miscDiceHalved) / 2.0) + miscDice
-        }else{
-          totalDice = baseDice + miscDice;
+          dialog_content = await renderTemplate("systems/ivys_torchbearer/templates/rolls/roll-build-dialog-beginners_luck.hbs", dataset);
+        }else{        
+          dataset.baseDice = this.system.rating
+        
+          dialog_content = await renderTemplate("systems/ivys_torchbearer/templates/rolls/roll-build-dialog.hbs", dataset);
         }
 
-        // let label = dataset.label ? `[ability] ${dataset.label}` : '';
-        let label = "";
-        if(roll_dialog_data.label){
-          label = label.concat(`Rolling ${roll_dialog_data.label}: ${totalDice}D`);
+
+        new Dialog({
+          title: "Rolling " + this.name,
+          content: dialog_content,
+          buttons: {
+            button1: {
+              label: "Roll",
+              callback: (html) => roll_dialog_callback(html, dataset, this.actor, this.system.beginners_luck),
+              icon: `<i class="fas fa-check"></i>`
+            },
+            button2: {
+              label: "Cancel",
+              callback: () => {},
+              icon: `<i class="fas fa-times"></i>`
+            }
+          }
+        }).render(true);
+
+        async function roll_dialog_callback(html, roll_dialog_data, actor, beginners_luck){
+          const ob = parseInt(html.find("input#ob").val());
+          const miscDice = parseInt(html.find("input#miscDice").val());
+          const miscSuccesses = parseInt(html.find("input#miscSuccesses").val());
+          const baseDice = parseInt(roll_dialog_data.baseDice);
+          let totalDice = 0;
+          if(beginners_luck){
+            const miscDiceHalved = parseInt(html.find("input#miscDiceHalved").val());
+            totalDice = Math.ceil((baseDice + miscDiceHalved) / 2.0) + miscDice
+          }else{
+            totalDice = baseDice + miscDice;
+          }
+
+          // let label = dataset.label ? `[ability] ${dataset.label}` : '';
+          let label = "";
+          if(roll_dialog_data.label){
+            label = label.concat(`Rolling ${roll_dialog_data.label}: ${totalDice}D`);
+          }
+          if(miscSuccesses){
+            label.concat(` +${miscSuccesses}S`);
+          }
+
+          let formula = `{${totalDice}d6cs>3}>=ob`;
+
+          let roll = new Roll(formula, actor.getRollData());
+
+          await roll.evaluate();
+
+          let rolled_successes = 0;
+          let dice = [];
+
+          roll.dice[0].results.forEach((die) => {
+            rolled_successes += die.count;
+            dice.push(die);
+          });
+
+          let succeeded = false;
+          let successes = rolled_successes;
+          if(rolled_successes >= ob){
+            succeeded = true;
+            successes += miscSuccesses;
+          }
+
+          let message_data = {
+            ob: ob,
+            dice: totalDice,
+            additional_successes: miscSuccesses,
+            label: label,
+            formula: formula,
+            rolled_successes: rolled_successes,
+            miscSuccesses: miscSuccesses,
+            succeeded: succeeded,
+            successes: successes,
+            dice: dice
+          }
+          const chat_message_content = await renderTemplate("systems/ivys_torchbearer/templates/rolls/roll-chat_message.hbs", message_data);
+          ChatMessage.create({
+            speaker: ChatMessage.getSpeaker({actor: actor}),
+            content: chat_message_content,
+            type: CONST.CHAT_MESSAGE_TYPES.ROLL
+          });
+
+          // roll.toMessage({
+          //   speaker: ChatMessage.getSpeaker({ actor: actor }),
+          //   flavor: label,
+          //   rollMode: game.settings.get('core', 'rollMode'),
+          // });
+          return roll;
         }
-        if(miscSuccesses){
-          label.concat(` +${miscSuccesses}S`);
+
+        break;
+      
+        case "Level Feature":
+          ChatMessage.create({
+            speaker: speaker,
+            rollMode: rollMode,
+            content: "<h3>" + item.name + "</h3>" + item.system.description
+          });
+  
+          break;
+      
+          case "Spell":
+            ChatMessage.create({
+              speaker: speaker,
+              rollMode: rollMode,
+              content: "<h3>" + item.name + "</h3>" + item.system.description
+            });
+    
+            break;
+      
+          case "Invocation":
+            ChatMessage.create({
+              speaker: speaker,
+              rollMode: rollMode,
+              content: "<h3>" + item.name + "</h3>" + item.system.description
+            });
+    
+            break;
+      
+          case "NPC":
+            ChatMessage.create({
+              speaker: speaker,
+              rollMode: rollMode,
+              content: "<h3>" + item.name + "</h3>" + "<i>" + item.system.relationship + "</i><br>" + item.system.description
+            });
+    
+            break;
+      
+      default:
+        // If there's no roll data, send a chat message.
+        if (!this.system.formula) {
+          ChatMessage.create({
+            speaker: speaker,
+            rollMode: rollMode,
+            flavor: label,
+            content: item.system.description ?? ''
+          });
         }
+        // Otherwise, create a roll and send a chat message from it.
+        else {
+          // Retrieve roll data.
+          const rollData = this.getRollData();
 
-        let formula = `{${totalDice}d6cs>3}>=ob`;
-
-        let roll = new Roll(formula, actor.getRollData());
-
-        await roll.evaluate();
-
-        let rolled_successes = 0;
-        let dice = [];
-
-        roll.dice[0].results.forEach((die) => {
-          rolled_successes += die.count;
-          dice.push(die);
-        });
-
-        let succeeded = false;
-        let successes = rolled_successes;
-        if(rolled_successes >= ob){
-          succeeded = true;
-          successes += miscSuccesses;
+          // Invoke the roll and submit it to chat.
+          const roll = new Roll(rollData.item.formula, rollData);
+          // If you need to store the value first, uncomment the next line.
+          // let result = await roll.roll({async: true});
+          roll.toMessage({
+            speaker: speaker,
+            rollMode: rollMode,
+            flavor: label,
+          });
+          return roll;
         }
-
-        let message_data = {
-          ob: ob,
-          dice: totalDice,
-          additional_successes: miscSuccesses,
-          label: label,
-          formula: formula,
-          rolled_successes: rolled_successes,
-          miscSuccesses: miscSuccesses,
-          succeeded: succeeded,
-          successes: successes,
-          dice: dice
-        }
-        const chat_message_content = await renderTemplate("systems/ivys_torchbearer/templates/rolls/roll-chat_message.hbs", message_data);
-        ChatMessage.create({
-          speaker: ChatMessage.getSpeaker({actor: actor}),
-          content: chat_message_content,
-          type: CONST.CHAT_MESSAGE_TYPES.ROLL
-        });
-
-        // roll.toMessage({
-        //   speaker: ChatMessage.getSpeaker({ actor: actor }),
-        //   flavor: label,
-        //   rollMode: game.settings.get('core', 'rollMode'),
-        // });
-        return roll;
-      }
-
-
-
-
-
-
-
-      return;
-    }
-
-    // If there's no roll data, send a chat message.
-    if (!this.system.formula) {
-      ChatMessage.create({
-        speaker: speaker,
-        rollMode: rollMode,
-        flavor: label,
-        content: item.system.description ?? ''
-      });
-    }
-    // Otherwise, create a roll and send a chat message from it.
-    else {
-      // Retrieve roll data.
-      const rollData = this.getRollData();
-
-      // Invoke the roll and submit it to chat.
-      const roll = new Roll(rollData.item.formula, rollData);
-      // If you need to store the value first, uncomment the next line.
-      // let result = await roll.roll({async: true});
-      roll.toMessage({
-        speaker: speaker,
-        rollMode: rollMode,
-        flavor: label,
-      });
-      return roll;
     }
   }
 }
